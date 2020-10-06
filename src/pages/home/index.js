6e836557c8e5fd995862841818b895b28ea16c74
Taylor Kettle <tin.teapot@gmail.com>
2020-10-06 23:40:13 +1300

    removed unused images and css classes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,21 +1,11 @@
 import React, { Component } from "react";
 
 import "./index.css";
-import IconLink from "../../components/icon-link";
 import HeroImage from "../../components/hero-image";
 import TextHeader from "../../components/text-header";
 import TextBlock from "../../components/text-block";
 import BodyText from "../../components/body-text";
 import recycleCentre from "../../img/recycle-centre.jpg";
-
-import cityToFarm from "../../img/city-to-farm.jpg";
-import kindergarten from "../../img/kindergarten.jpg";
-import school from "../../img/school.jpg";
-import shop from "../../img/shop.jpg";
-import transitionTowns from "../../img/transition-towns.jpg";
-import whangaparaoa2 from "../../img/whangaparaoa-2.jpg";
-import whangaparaoa from "../../img/whangaparaoa.jpg";
-import zeroWasteEvents from "../../img/zero-waste-events.jpg";
 import ProjectsPage from "../projects";
 
 export default class HomePage extends Component {
